a9aa61ec02ddef9ac523e2b6df81b07f729e635d
J._.N._.TAPANIYA <jenish.tapaniya123@gmail.com>
2026-06-27 18:53:49 +0530

    Add files via upload

---
+++ del_statement_.py
@@ -0,0 +1,7 @@
+a=[1,2,3,4,5]
+del a[1]
+print(a)
+print(" ")
+a=[1,2,3,4,5,6]
+a.clear()
+print(a)
---
+++ list_datatyp_.py
@@ -0,0 +1,15 @@
+a=['apple','grapes','charry','mengo','banana']
+for item in a:
+    print(item)
+print(" ")
+a=[[1,2],[3,4]]
+print(a[0])
+print(a[1][0])
+print(" ")
+votes=['no','no','yes','yes','no']
+total=votes.count('yes')
+print(total)
+print(" ")
+scores=[2,5,7,8,1,3,9]
+scores.sort()
+print(scores)
